8ee76f7b6abf96a39aaa2dcf7a5a60dda3512983
kwon <103391269+ctk-001@users.noreply.github.com>
2023-03-03 11:42:12 +0900

    Update run_process.sh

--- deploy/scripts/run_process.sh
+++ deploy/scripts/run_process.sh
@@ -13,3 +13,4 @@ aws ecr get-login-password --region ${REGION} \
 export IMAGE=${ECR_DOCKER_IMAGE};
 export TAG=${ECR_DOCKER_TAG};
 docker-compose -f /home/ubuntu/docker-compose.yml up -d --build;
+sudo rm -rf /home/ubuntu/scripts/run_process.sh
