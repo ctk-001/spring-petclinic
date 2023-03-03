dcb4d5c61cbceffbec44b763df28ccbf091bf173
kwon <103391269+ctk-001@users.noreply.github.com>
2023-03-03 15:13:28 +0900

    Update kill.sh

--- kill.sh
+++ kill.sh
@@ -4,4 +4,5 @@ echo "kill all"
 
 sudo rm -rf /home/ubuntu/scripts/kill_process.sh
 sudo rm -rf /home/ubuntu/scripts/run_process.sh
+sudo rm -rf /home/ubuntu/docker-compose.yml
 sudo rm -rf /home/ubuntu/*
